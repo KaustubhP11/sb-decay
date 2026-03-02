c77ff46fa3620811a980fd16095b3826611b8e0f
KaustubhP11 <ponkshekaustubh11@gmail.com>
2026-03-02 14:12:47 +0100

    modified runs and evals

--- text/evaluation/smollm3/basic_eval.sh
+++ text/evaluation/smollm3/basic_eval.sh
@@ -3,11 +3,11 @@ set -euo pipefail
 
 # Run basic evals on the model
 
-MODEL_PATH="/iopsstor/scratch/cscs/kponkshe/sb-decay/checkpoints/run_stable_openthoughts/hf"
+MODEL_PATH="/iopsstor/scratch/cscs/kponkshe/sb-decay/checkpoints/run_decay_dolma/hf"
 # Run basic evals on the model
 lm_eval --model vllm \
     --model_args pretrained="${MODEL_PATH}",tensor_parallel_size=1,dtype=auto,gpu_memory_utilization=0.8,data_parallel_size=1 \
     --tasks mmlu,piqa,hellaswag,arc_challenge,arc_easy,openbookqa,commonsense_qa \
-    --output_path /iopsstor/scratch/cscs/kponkshe/sb-decay/checkpoints/run_stable_openthoughts/hf/aggregated_results.json \
+    --output_path "${MODEL_PATH}/aggregated_results.json" \
     --batch_size auto
 
