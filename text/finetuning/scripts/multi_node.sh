b10649d0382c8e6d331031c74ba3e9928042e97b
KaustubhP11 <ponkshekaustubh11@gmail.com>
2026-03-12 17:39:09 +0100

    slurm script corrections

--- text/finetuning/scripts/multi_node.sh
+++ text/finetuning/scripts/multi_node.sh
@@ -4,17 +4,18 @@ set -euo pipefail
 ACCELERATE_CONFIG="${ACCELERATE_CONFIG:-text/finetuning/configs/zero3.yaml}"
 TRAIN_CONFIG="${TRAIN_CONFIG:-text/finetuning/configs/sft_full.yaml}"
 
-GPUS_PER_NODE="${GPUS_PER_NODE:-4}"
-NNODES="${NNODES:-1}"
-NODE_RANK="${NODE_RANK:-${SLURM_NODEID:-0}}"
-MASTER_ADDR="${MASTER_ADDR:-127.0.0.1}"
-MASTER_PORT="${MASTER_PORT:-29500}"
+ACCEL_PROCS=$(( $SLURM_NNODES * $SLURM_GPUS_PER_NODE ))
+
+MASTER_ADDR=$(scontrol show hostnames $SLURM_NODELIST | head -n1)
+MASTER_PORT=12802
+
+
 
 accelerate launch \
   --config_file "${ACCELERATE_CONFIG}" \
-  --num_processes "$((GPUS_PER_NODE * NNODES))" \
-  --num_machines "${NNODES}" \
-  --machine_rank "${NODE_RANK}" \
+  --num_processes "$ACCEL_PROCS" \
+  --num_machines "${SLURM_NNODES}" \
+  --machine_rank $SLURM_PROCID \
   --main_process_ip "${MASTER_ADDR}" \
   --main_process_port "${MASTER_PORT}" \
   text/finetuning/sft_train.py \
--- text/finetuning/scripts/submit_multinode.sh
+++ text/finetuning/scripts/submit_multinode.sh
@@ -1,21 +1,12 @@
-#!/bin/bash -l
+#!/bin/bash
 #SBATCH --job-name=smollm3-sft
 #SBATCH --nodes=4
 #SBATCH --ntasks-per-node=1
-#SBATCH --gres=gpu:4
+#SBATCH --gpus-per-node=4
 #SBATCH --qos=normal
-#SBATCH --time=11:30:00
+#SBATCH --time=00:30:00
 #SBATCH --output=./logs/train-%j.out
 #SBATCH --error=./logs/train-%j.err
 
-set -euo pipefail
-mkdir -p logs
 
-export GPUS_PER_NODE="${SLURM_GPUS_ON_NODE:-4}"
-export NNODES="${SLURM_NNODES:-1}"
-export MASTER_PORT="${MASTER_PORT:-29500}"
-export MASTER_ADDR="${MASTER_ADDR:-$(scontrol show hostnames "${SLURM_JOB_NODELIST}" | head -n 1)}"
-export NODE_RANK="${NODE_RANK:-${SLURM_NODEID:-0}}"
-
-export OMP_NUM_THREADS="${OMP_NUM_THREADS:-4}"
 bash text/finetuning/scripts/multi_node.sh
